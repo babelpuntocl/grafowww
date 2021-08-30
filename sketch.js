f1da1bfb31d4f11fd5fe934595228af3c531c538
“Nicolas <hola@babel.cl>
2021-08-30 00:57:38 -0400

    update

--- sketch.js
+++ sketch.js
@@ -32,6 +32,7 @@ const c2 = ( canvas2 ) => {
   canvas2.setup = () => {
     canvas2.createCanvas(w,h);
     canvas2.fill(0)
+    canvas2.rectMode(canvas2.CENTER)
   };
   canvas2.draw = () => {
     canvas2.background(200);
@@ -41,7 +42,7 @@ const c2 = ( canvas2 ) => {
     let targetY = canvas2.mouseY;
     let dy = targetY - y;
     y += dy * easing;
-    canvas2.ellipse(x, y, 50,50);
+    canvas2.rect(x, y, 50,50);
   };
 };
 
@@ -68,10 +69,10 @@ const c3 = ( canvas3 ) => {
     canvas3.text("random", X, Y);
     canvas3.textSize(Z);
   };
-  // canvas3.mousePressed = () => {
-  // // set canvas background will "clear" the canvas
-  //   canvas3.background(255);
-  // };
+  canvas3.mousePressed = () => {
+  // set canvas background will "clear" the canvas
+    canvas3.background(255);
+  };
 };
 
 let myp5c3 = new p5(c3,document.getElementById('p5Canvas3'));
@@ -84,17 +85,34 @@ let myp5c3 = new p5(c3,document.getElementById('p5Canvas3'));
 
 
 const c4 = ( canvas4 ) => {
-  var scribble = new Scribble( canvas4 );
+  var startTime = 0;
+  var ellipseSize = 50;
+  var tiempoBase = 500;
   // canvas4.preload = () => {
   //   img = canvas4.loadImage('pics/image.jpg');
   // };
   canvas4.setup = () => {
     canvas4.createCanvas(w,h);
-    canvas4.imageMode(canvas4.CENTER);
-    scribble.scribbleLine( 0, 0, canvas4.width, canvas4.height );
+    canvas4.ellipseMode(canvas4.CORNER);
+    canvas4.frameRate(3);
   };
   canvas4.draw = () => {
+    canvas4.background(255);
+    var randWidth = canvas4.random(0,canvas4.width);
+    var randHeight = canvas4.random(0,canvas4.height);
     //canvas4.image(img, canvas4.width/2, canvas4.height/2, img.width, img.height);
+    if (canvas4.millis() - startTime > tiempoBase) {
+    	 canvas4.ellipse(randWidth, randHeight, ellipseSize, ellipseSize);
+    };
+    if (canvas4.millis() - startTime  > tiempoBase*2) {
+    	 canvas4.ellipse(randWidth, randHeight, ellipseSize, ellipseSize);
+    };
+    if (canvas4.millis() - startTime  > tiempoBase*3) {
+    	 canvas4.ellipse(randWidth, randHeight, ellipseSize, ellipseSize);
+    };
+    if (canvas4.millis() - startTime > tiempoBase*4) {
+    	 startTime = canvas4.millis();
+    };
   };
 };
 
